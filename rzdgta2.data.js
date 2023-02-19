5498c019c9d5fb48e263c9b7ea5c77088876651b
PBolsz <111749731+PBolsz@users.noreply.github.com>
2023-02-19 20:51:51 +0700

    Update rzdgta2.data.js

--- rzdgta2.data.js
+++ rzdgta2.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'T:/UE projects/rzdgta2/Binaries/HTML5/rzdgta2.data';
-    var REMOTE_PACKAGE_BASE = 'rzdgta2.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/47ek406f3gre5h2/rzdgta2.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/47ek406f3gre5h2/rzdgta2.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
